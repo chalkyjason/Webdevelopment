f19586e8f5f66a7fcb149be800853bc5a353978c
army_jay <chalkyjason@gmail.com>
2022-05-30 21:30:56 -0400

    update website colors and DFX page

--- ICPforDummies/sidebar.js
+++ ICPforDummies/sidebar.js
@@ -11,3 +11,19 @@ function closeNav() {
   document.getElementById("main").style.marginLeft = "0";
   document.body.style.backgroundColor = "black";
 }
+
+//* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
